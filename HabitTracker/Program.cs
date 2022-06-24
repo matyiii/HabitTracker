adb4dccdf99c3f2a98ac674ed1cf326234898e11
matyiii <mezofimate@gmail.com>
2022-06-24 14:44:45 +0200

    connectionString, create table

--- HabitTracker/Program.cs
+++ HabitTracker/Program.cs
@@ -1,4 +1,34 @@
-﻿Console.WriteLine("What would you like to do?");
+﻿using Microsoft.Data.Sqlite;
+
+string connectionString = @"Data Source=habit-Tracker.db";
+
+CreateDatabase();
+
+void CreateDatabase()
+{
+    using (var connection = new SqliteConnection(connectionString))
+    {
+        using (var tableCmd = connection.CreateCommand())
+        {
+            connection.Open();
+
+            tableCmd.CommandText = @"CREATE TABLE IF NOT EXISTS myHabit (
+                                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    Date TEXT,
+                                    Quantity INTEGER)";
+            tableCmd.ExecuteNonQuery(); //végrehajtja a parancsot ami nem lekérdezés és nem ad vissza adatot
+        }
+    }
+}
+
+Console.WriteLine("What would you like to do?");
+
+Console.WriteLine("Type 0 to Close the App.");
+Console.WriteLine("Type 1 to View All Records.");
+Console.WriteLine("Type 2 to Insert Record.");
+Console.WriteLine("Type 3 to Update Record.");
+Console.WriteLine("Type 4 to Delete Record.");
+
 switch (Console.ReadLine())
 {
     case "0":

6fe1dc83774490d8e17d7c4ee71c048f4fd41a81
matyiii <mezofimate@gmail.com>
2022-06-24 15:03:56 +0200

    CreateDatabase working, GetUserInput function

--- HabitTracker/Program.cs
+++ HabitTracker/Program.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.Data.Sqlite;
+﻿using Microsoft.Data.Sqlite; //Microsoft.Data.Sqlite nuget is kell nem cska a core
 
 string connectionString = @"Data Source=habit-Tracker.db";
 
 CreateDatabase();
+
+GetUserInput();
+Console.ReadLine();
 
 void CreateDatabase()
 {
@@ -18,26 +21,29 @@ void CreateDatabase()
                                     Quantity INTEGER)";
             tableCmd.ExecuteNonQuery(); //végrehajtja a parancsot ami nem lekérdezés és nem ad vissza adatot
         }
+        //a using zárja a kapcsolatot nem kell külön
     }
 }
 
-Console.WriteLine("What would you like to do?");
-
-Console.WriteLine("Type 0 to Close the App.");
-Console.WriteLine("Type 1 to View All Records.");
-Console.WriteLine("Type 2 to Insert Record.");
-Console.WriteLine("Type 3 to Update Record.");
-Console.WriteLine("Type 4 to Delete Record.");
-
-switch (Console.ReadLine())
+void GetUserInput()
 {
-    case "0":
-        Console.WriteLine("TESZT0");
-        break;
-    case "1":
-        Console.WriteLine("TESZT1");
-        break;
-    default:
-        break;
+    Console.WriteLine("What would you like to do?");
+
+    Console.WriteLine("Type 0 to Close the App.");
+    Console.WriteLine("Type 1 to View All Records.");
+    Console.WriteLine("Type 2 to Insert Record.");
+    Console.WriteLine("Type 3 to Update Record.");
+    Console.WriteLine("Type 4 to Delete Record.");
+
+    switch (Console.ReadLine())
+    {
+        case "0":
+            Console.WriteLine("TESZT0");
+            break;
+        case "1":
+            Console.WriteLine("TESZT1");
+            break;
+        default:
+            break;
+    }
 }
-Console.ReadLine();
